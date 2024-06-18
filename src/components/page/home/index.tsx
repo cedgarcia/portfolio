a1303786254510d8a34307dbbdb685ee9fa69807
Cedrick Garcia <cedrick.garcia@bluehive.com.ph>
2024-06-19 00:28:40 +0800

    add footer

--- src/components/page/home/index.tsx
+++ src/components/page/home/index.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import Navigation from "@/components/common/Navigation";
 import HomePageDesktop from "./sections/Desktop";
+import Footer from "@/components/common/Footer";
 
 const index: React.FC = () => {
   return (
@@ -8,9 +9,10 @@ const index: React.FC = () => {
       <div className="mx-auto max-w-[1100px] px-[10px]">
         <Navigation />
       </div>
-      <div className="hidden laptop:flex">
+      <div className="hidden laptop:block">
         <HomePageDesktop />
       </div>
+      <Footer />
     </>
   );
 };
