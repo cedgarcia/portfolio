fb148bbd7d213945daa87d96e5c9d2e3dbb79672
Cedrick Garcia <cedrick.garcia@bluehive.com.ph>
2024-06-18 14:29:08 +0800

    Change

--- src/components/page/home/sections/LandingSection.tsx
+++ src/components/page/home/sections/LandingSection.tsx
@@ -1,4 +1,5 @@
 import ShuffleTextPage from "@/components/common/ShuffleTextPage";
+import ShuffleTextSlow from "@/components/common/ShuffleTextSlow";
 import React from "react";
 
 const LandingSection = () => {
@@ -11,12 +12,17 @@ const LandingSection = () => {
         <div className="font-heading3 text-[20px] text-blue-100">
           <ShuffleTextPage text="Hi, I'm" />
         </div>
-        <div className="flex flex-col font-heading1 text-[6rem] text-blue-300">
-          <ShuffleTextPage text="Ced Garcia" />
+        <div className="flex flex-row font-heading1 text-[6rem] text-blue-300">
+          <span className="mr-5">
+            <ShuffleTextSlow text="Ced" />
+          </span>
+          <span>
+            <ShuffleTextSlow text="Garcia" />
+          </span>
         </div>
         <div className="">
           <div className="font-heading3 text-[2.5rem] text-blue-400">
-            <ShuffleTextPage text="I create digital" />
+            <ShuffleTextPage text="I build digital" />
           </div>
           <div className="font-heading3 text-[3rem] text-blue-500">
             <ShuffleTextPage text="solutions for the web." />
