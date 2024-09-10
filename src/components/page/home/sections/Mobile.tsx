a9519f77d7d0bcf72d2e00bcd6458292a3480589
Cedrick Garcia <icedgarcia@gmail.com>
2024-09-10 23:14:29 +0800

    hide cv

--- src/components/page/home/sections/Mobile.tsx
+++ src/components/page/home/sections/Mobile.tsx
@@ -76,7 +76,7 @@ const Mobile = () => {
             </Link>
           </span>
         </div>
-        <Link
+        {/* <Link
           target="__blank"
           href="https://drive.google.com/file/d/1Rq_CG8o35T16brTgSbqfB6NT3JY303RX/view"
           className="mt-10 w-fit "
@@ -84,7 +84,7 @@ const Mobile = () => {
           <div className="w-full border-2 px-5 py-2 font-heading2 text-blue-300 duration-300 ease-in-out  hover:border-blue-500 hover:text-blue-500">
             <ShuffleTextPage text="Download CV" />
           </div>
-        </Link>
+        </Link> */}
       </section>
       {/* Projects Section */}
       <section className="  ">
